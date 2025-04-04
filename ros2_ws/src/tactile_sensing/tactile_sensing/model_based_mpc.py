743ffdaa04e51559bd543e5ef2b237d4d1ac2e29
leonelgiacobbe <lgiacobbe14@gmail.com>
2025-04-04 12:33:46 -0500

    Moved unchanged calculations out of run loop

--- ros2_ws/src/tactile_sensing/tactile_sensing/model_based_mpc.py
+++ ros2_ws/src/tactile_sensing/tactile_sensing/model_based_mpc.py
@@ -99,7 +99,6 @@ class ModelBasedMPCNode(Node):
             tty.setcbreak(sys.stdin.fileno())
         else:
             self.old_attr = None
-        
 
         # Parameters initialization
         self.frequency = 60
@@ -126,8 +125,96 @@ class ModelBasedMPCNode(Node):
         self.gripper_cmd.command.max_effort = 100.0  # Set max effort
         self.rate = self.create_rate(self.frequency)
 
+        # Initialize all constant MPC components
+        self._initialize_mpc_components()
+
         # Timer to call the run method periodically
         self.timer = self.create_timer(1.0 / self.frequency, self.run)
+
+    def _initialize_mpc_components(self):
+        """Initialize all constant MPC components that don't change during execution"""
+        # reference to track
+        self.r = np.array([self.c_ref, 0, 0, 0])
+        self.r_ = vstack_help(self.r, self.N)
+
+        # model
+        self.Ad = sparse.csc_matrix([
+            [1, 0, 0, self.k_c * self.del_t],
+            [0, 1, 0, 0],
+            [0, 0, 1, -self.del_t],
+            [0, 0, 0, 1]
+        ])
+
+        self.Bd = sparse.csc_matrix([
+            [0],
+            [0],
+            [-0.5 * self.del_t * self.del_t],
+            [self.del_t]
+        ])
+
+        # weights
+        self.Q = sparse.csc_matrix([
+            [self.q_c, self.q_c * self.q_d, 0, 0],
+            [self.q_c * self.q_d, self.q_c * (self.q_d ** 2), 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, self.q_v]
+        ])
+        self.R = self.q_a * sparse.eye(1)
+        self.QN = self.p * self.Q
+        self.Q_ = sparse.block_diag([sparse.kron(sparse.eye(self.N - 1), self.Q), self.QN], format='csc')
+        self.R_ = sparse.block_diag([sparse.kron(sparse.eye(self.N), self.R)], format='csc')
+
+        # T initialization
+        self.T_ = self.Ad
+        temp = self.Ad
+        for i in range(self.N - 1):
+            temp = temp.dot(self.Ad)
+            self.T_ = sparse.vstack([self.T_, temp])
+
+        I = sparse.eye(self.dim)
+        row_single = zeros_hstack_help(I, self.N, self.dim, self.dim)
+        self.AN_ = row_single
+        for i in range(self.N - 1):
+            AN = I
+            row_single = I
+            for j in range(i + 1):
+                AN = self.Ad.dot(AN)
+                row_single = sparse.hstack([AN, row_single])
+            row_single = zeros_hstack_help(row_single, self.N - i - 1, self.dim, self.dim)
+            self.AN_ = sparse.vstack([self.AN_, row_single])
+
+        self.Bd_ = sparse.block_diag([sparse.kron(sparse.eye(self.N), self.Bd)])
+        self.S_ = self.AN_ * self.Bd_
+
+        # vel and acc constraints
+        self.max_con_b = (np.array([self.vel_max])).reshape(1, 1)
+        self.min_con_b = (np.array([-self.vel_max])).reshape(1, 1)
+        self.u_max = self.acc_max * np.ones(1 * self.N)
+        self.u_max = self.u_max.reshape(1 * self.N, 1)
+
+        self.max_con_b_ = vstack_help(self.max_con_b, self.N)
+        self.min_con_b_ = vstack_help(self.min_con_b, self.N)
+
+        # vel select matrix
+        self.C_con = sparse.csc_matrix([
+            [0, 0, 0, 1]
+        ])
+
+        self.C_con_T_ = getCT_(self.C_con, self.T_)
+
+        # select matrix cost function
+        self.L = sparse.eye(self.dim)
+        self.L_ = sparse.block_diag([sparse.kron(sparse.eye(self.N), self.L)], format='csc')
+
+        # QP setup matrices
+        self.P_ = 2 * (self.R_ + (self.S_.T) * (self.L_.T) * self.Q_ * self.L_ * self.S_)
+        self.A_ = sparse.vstack([sparse.block_diag([sparse.eye(1 * self.N)], format='csc'), getCS_(self.C_con, self.S_)])
+
+        # Initialize OSQP problem
+        self.prob = osqp.OSQP()
+        self.prob.setup(self.P_, np.zeros(self.P_.shape[0]), self.A_, 
+                      np.zeros(self.A_.shape[0]), np.zeros(self.A_.shape[0]), 
+                      warm_start=True, max_iter=4000)
 
     # Receives position of gripper: 0.0 -> completely open. 0.8 -> completely closed
     def joint_state_cb(self, msg: JointState): 
@@ -157,101 +244,10 @@ class ModelBasedMPCNode(Node):
         try:
             # Wait until gripper posi callback is called once
             wait_for_message(JointState, self, '/joint_states', time_to_wait=10.0)
-
             wait_for_message(Float32, self, '/gs_contact_area', time_to_wait=10.0)
 
-            # state and control Initialization
+            # Initial state
             x_state = np.array([0., 0., 0., 0.])
-            u0 = np.array([[0.]])
-
-            # reference to track
-            r = np.array([self.c_ref, 0, 0, 0])
-            r_ = vstack_help(r, self.N)
-
-            # model
-            Ad = sparse.csc_matrix([
-                [1, 0, 0, self.k_c * self.del_t],
-                [0, 1, 0, 0],
-                [0, 0, 1, -self.del_t],
-                [0, 0, 0, 1]
-            ])
-
-            Bd = sparse.csc_matrix([
-                [0],
-                [0],
-                [-0.5 * self.del_t * self.del_t],
-                [self.del_t]
-            ])
-
-            # weights
-            Q = sparse.csc_matrix([
-                [self.q_c, self.q_c * self.q_d, 0, 0],
-                [self.q_c * self.q_d, self.q_c * (self.q_d ** 2), 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, self.q_v]
-            ])
-            R = self.q_a * sparse.eye(1)
-            QN = self.p * Q
-            Q_ = sparse.block_diag([sparse.kron(sparse.eye(self.N - 1), Q), QN], format='csc')
-            R_ = sparse.block_diag([sparse.kron(sparse.eye(self.N), R)], format='csc')
-
-            # T initialization
-            T_ = Ad
-            temp = Ad
-            for i in range(self.N - 1):
-                temp = temp.dot(Ad)
-                T_ = sparse.vstack([T_, temp])
-
-            I = sparse.eye(self.dim)
-            row_single = zeros_hstack_help(I, self.N, self.dim, self.dim)
-            AN_ = row_single
-            for i in range(self.N - 1):
-                AN = I
-                row_single = I
-                for j in range(i + 1):
-                    AN = Ad.dot(AN)
-                    row_single = sparse.hstack([AN, row_single])
-                row_single = zeros_hstack_help(row_single, self.N - i - 1, self.dim, self.dim)
-                AN_ = sparse.vstack([AN_, row_single])
-
-            Bd_ = sparse.block_diag([sparse.kron(sparse.eye(self.N), Bd)])
-            S_ = AN_ * Bd_
-
-            # vel and acc constraints
-            max_con_b = (np.array([self.vel_max])).reshape(1, 1)
-            min_con_b = (np.array([-self.vel_max])).reshape(1, 1)
-            u_max = self.acc_max * np.ones(1 * self.N)
-            u_max = u_max.reshape(1 * self.N, 1)
-
-            max_con_b_ = vstack_help(max_con_b, self.N)
-            min_con_b_ = vstack_help(min_con_b, self.N)
-
-            # vel select matrix
-            C_con = sparse.csc_matrix([
-                [0, 0, 0, 1]
-            ])
-
-            C_con_T_ = getCT_(C_con, T_)
-
-            # real-time vel bounds
-            max_con_b_update = b_CT_x0(max_con_b_, C_con_T_, x_state.reshape(self.dim, 1))
-            min_con_b_update = b_CT_x0(min_con_b_, C_con_T_, x_state.reshape(self.dim, 1))
-
-            u_ = np.vstack([u_max, max_con_b_update])
-            l_ = np.vstack([u_max * -1, min_con_b_update])
-
-            # select matrix cost function
-            L = sparse.eye(self.dim)
-            L_ = sparse.block_diag([sparse.kron(sparse.eye(self.N), L)], format='csc')
-
-            # QP setup
-            P_ = 2 * (R_ + (S_.T) * (L_.T) * Q_ * L_ * S_)
-            q_ = 2 * (x_state.reshape(1, self.dim) * (T_.T) * (L_.T) - r_.T) * Q_ * L_ * S_
-            A_ = sparse.vstack([sparse.block_diag([sparse.eye(1 * self.N)], format='csc'), getCS_(C_con, S_)])
-
-            prob = osqp.OSQP()
-            prob.setup(P_, q_.T, A_, l_, u_, warm_start=True, max_iter=8000)
-            initial_flag = False
 
             while rclpy.ok():
                 if x_state[2] == 0.:
@@ -267,15 +263,15 @@ class ModelBasedMPCNode(Node):
                         x_state = np.array([self.contact_area_, 0, x_state[2], x_state[3]]) # change -self.dis_sum_ to 0
 
                 # constraints update
-                max_con_b_update = b_CT_x0(max_con_b_, C_con_T_, x_state.reshape(self.dim, 1))
-                min_con_b_update = b_CT_x0(min_con_b_, C_con_T_, x_state.reshape(self.dim, 1))
-                u_ = np.vstack([u_max, max_con_b_update])
-                l_ = np.vstack([u_max * -1, min_con_b_update])
+                max_con_b_update = b_CT_x0(self.max_con_b_, self.C_con_T_, x_state.reshape(self.dim, 1))
+                min_con_b_update = b_CT_x0(self.min_con_b_, self.C_con_T_, x_state.reshape(self.dim, 1))
+                u_ = np.vstack([self.u_max, max_con_b_update])
+                l_ = np.vstack([self.u_max * -1, min_con_b_update])
 
                 # QP update
-                q_ = 2 * (x_state.reshape(1, self.dim) * (T_.T) * (L_.T) - r_.T) * Q_ * L_ * S_
-                prob.update(q=q_.T, l=l_, u=u_)
-                res = prob.solve()
+                q_ = 2 * (x_state.reshape(1, self.dim) * (self.T_.T) * (self.L_.T) - self.r_.T) * self.Q_ * self.L_ * self.S_
+                self.prob.update(q=q_.T, l=l_, u=u_)
+                res = self.prob.solve()
                 ctrl = res.x[0:1].copy()
 
                 self.gripper_cmd = GripperCommand.Goal()
@@ -283,12 +279,13 @@ class ModelBasedMPCNode(Node):
 
                 if ctrl[0] is not None:
                     # p, v update
-                    x_state = Ad.dot(x_state) + Bd.dot(ctrl)
+                    x_state = self.Ad.dot(x_state) + self.Bd.dot(ctrl)
                     normalized_x_state = self.new_min + ((x_state[2] - self.lower_pos_lim) / (self.upper_pos_lim - self.lower_pos_lim)) * (self.new_max - self.new_min)
                     print("x_state_2 ", abs(normalized_x_state))
                     print("Current tactile value: ", self.contact_area_)
                     self.gripper_cmd.command.position = self.gripper_posi_ + abs(normalized_x_state)
                     self._action_client.send_goal_async(self.gripper_cmd)
+                
                 # Send goal to the action server
                 self._send_goal(self.gripper_cmd)
                 self.rate.sleep()
@@ -298,9 +295,7 @@ class ModelBasedMPCNode(Node):
             self.get_logger().info('Interrupted!')
 
     def _send_goal(self, goal):
-        # self.get_logger().info('Sending goal...')
         self._action_client.wait_for_server()
-        # print("Goal: ", goal)
         self._send_goal_future = self._action_client.send_goal_async(goal)
         self._send_goal_future.add_done_callback(self._goal_response_callback)
 
@@ -320,7 +315,7 @@ class ModelBasedMPCNode(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = ModelBasedMPCNode()
-    executor = MultiThreadedExecutor(num_threads=6)
+    executor = MultiThreadedExecutor(num_threads=10)
     executor.add_node(node)
     executor.spin()
 
